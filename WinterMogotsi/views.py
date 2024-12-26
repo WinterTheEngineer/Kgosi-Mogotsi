b68011f6e634746340583aa5598bfaf1e9d99ebe
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2024-12-26 02:11:10 +0200

    added contact view to process contact form, added env variables for security

--- WinterMogotsi/views.py
+++ WinterMogotsi/views.py
@@ -1,30 +1,57 @@
-from django.shortcuts import render
+import os
+from dotenv import load_dotenv
+from .forms import ContactForm
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
 
-
-# Create your views here.
+load_dotenv()
 
 def index(request):
 
     context = {
-        'page_title': 'Home'
+        'page_title': 'Home',
+        'form': ContactForm
     }
 
     return render(request, "WinterMogotsi/index.html", context)
 
-
-def education(request):
-
-    context = {
-        'page_title': 'Education'
-    }
-
-    return render(request, "WinterMogotsi/education.html", context)
-
-
 def contact(request):
 
-    context = {
+    if request.method == "POST":
 
-    }
+        form = ContactForm
+        if form.is_valid():
 
-    return render(request, "WinterMogotsi/contact.html", context)
+            # extract and process form data
+            name = form.cleaned_data['name']
+            service = form.cleaned_data['service']
+            email = form.cleaned_data['email']
+            phone = form.cleaned_data['phone']
+            message = form.cleaned_data['message']
+
+            # format e-mail content
+            subject = f"Contact Form Submission from {name}"
+            body = (
+                f"Name: {name}\n"
+                f"Service Interested: {service}\n"
+                f"Email: {email}\n"
+                f"Phone: {phone}\n\n"
+                f"Message:\n{message}"
+            )
+
+            sender = os.getenv("EMAIL")
+            recipient_list = [os.getenv("EMAIL")]
+
+            try:
+                send_mail(subject, body, sender, recipient_list)
+                messages.success(request, "Thanks for reaching out! I'll get back to you shortly.")
+            except Exception as e:
+                messages.error(request, f"An error occurred while sending your message. Try again later.")
+            
+            return redirect(index)
+        
+        else:
+            messages.error(request, "There was an error with your form. Please try again.")
+    
+    return redirect('index')
