2d1f673f3592bf0774ed41ab63276ce0710f666d
Rhulani Mogotsi <rhulanimogotsi@gmail.com>
2025-10-16 02:51:30 +0200

    added database config

--- KgosiMogotsi/settings.py
+++ KgosiMogotsi/settings.py
@@ -79,12 +79,24 @@ WSGI_APPLICATION = 'KgosiMogotsi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME', "postgres"),
+            'USER': os.getenv('DB_USER', "postgres"),
+            'PASSWORD': os.getenv('DB_PASS', ""),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT', default='5432'),
+        }
+    }
 
 
 # Password validation
